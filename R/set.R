a9b01b65b8c2096b0b7c7117b61fda0d3c07adf0
Chanyub Park <mrchypark@gmail.com>
2019-08-02 22:56:38 +0900

    symlink dont work

--- R/set.R
+++ R/set.R
@@ -1,5 +1,69 @@
-setx <- function(key, value){
-  sys::exec_wait("setx", args = c(key, value))
-  if (rstudioapi::hasFun("openProject"))
-    rstudioapi::openProject(".")
+set_java_home <- function(os, ...){
+  UseMethod("set_java_home")
 }
+
+set_java_home.default <- function(os){
+  stop("no method for ", class(os)[1L])
+}
+
+set_java_home.Windowsx86 <- function(os, path = ""){
+  if (path == "") {
+    path <- fs::dir_ls(crt_path(os))
+  }
+  if (lenght(path) == 0) {
+    stop("There's empty. Please install java first.")
+  }
+  if (lenght(path) > 1) {
+    
+  }
+  setx("JAVA_HOME", path)
+  message("You MUST restart rstudio for setting done.")
+}
+
+set_java_home.Windowsx64 <- function(os, path = ""){
+  if (path == "") {
+    path <- fs::path(crt_path(os), "current")
+  }
+  setx("JAVA_HOME", path)
+  message("You MUST restart rstudio for setting done.")
+}
+
+crt_path <- function(os) {
+  UseMethod("crt_path")
+}
+
+crt_path.default <- function(os){
+  stop("no method for ", class(os)[1L])
+}
+
+crt_path.Windowsx86 <- function(os){
+  fs::path(fs::path_home(),"corretto")
+}
+
+crt_path.Windowsx64 <- function(os){
+  fs::path(fs::path_home(),"corretto")
+}
+
+crt_path.Darwinx64 <- function(os){
+  fs::path("/Library/Java/JavaVirtualMachines/")
+}
+
+mklink <- function(link, target) {
+  R.utils::createLink(link, target, overwrite = T, methods = "windows-shortcut")
+}
+# mklink(fs::path(crt_path(get_os()),"current"), fs::dir_ls(crt_path(get_os())))
+
+setx <- function(key = "", value = ""){
+  if (key != "" & value == "") {
+    sys::exec_wait("setx", args = c(key))
+  } else {
+    sys::exec_wait("setx", args = c(key, value))
+  }
+}
+
+java_home_mac <- function(args = "") {
+  res <- sys::exec_wait("/usr/libexec/java_home", args = args)
+  invisible(res)
+}
+
+
--- R/uncompress.R
+++ R/uncompress.R
@@ -2,26 +2,23 @@ crt_unc <- function(os, ...){
   UseMethod("crt_unc")
 }
 
-crt_unc.Windowsx86 <- function(dest){
-  tar <- download_links()
-  download.file(tar, destfile = dest, mode = "wb")
-}
-
-crt_unc.Windowsx64 <- function(os, dest){
-  tar <- download_links()
-  download.file(tar, destfile = dest, mode = "wb")
-}
-
-crt_unc.Darwinx64 <- function(os, dest){
-  tar <- download_links()
-  download.file(tar, destfile = dest)
-}
-
-crt_unc.Linuxx64 <- function(os, dest){
-  tar <- download_links()
-  download.file(tar, destfile = dest)
-}
-
-crt_unc.default <- function(os, dest){
+crt_unc.default <- function(os){
   stop("no method for ", class(os)[1L])
 }
+
+crt_unc.Windowsx86 <- function(os, ...){
+  zip::unzip(...)
+}
+
+crt_unc.Windowsx64 <- function(os, ...){
+  zip::unzip(...)
+}
+
+# crt_unc.Darwinx64 <- function(os, dest){
+#  
+# }
+# 
+# crt_unc.Linuxx64 <- function(os, dest){
+#  
+# }
+# 
