eb50d4bbae51c9eb8ed8c590e56aaaffb7194fd5
Chanyub Park <mrchypark@gmail.com>
2019-08-07 21:14:57 +0900

    ask quit rstudio

--- R/install.R
+++ R/install.R
@@ -4,4 +4,13 @@ install_corretto <- function(os = get_os()){
   loc <- crt_path(os)
   crt_unc(os, dest, exdir = loc)
   set_java_home(os)
+  if (grepl("Windows", class(os))){
+    ans <- showQuestion("Set JAVA_HOME",
+                 "Set env process need to restart rstudio.\n Quit rstudio now?",
+                 "Now","later")
+  }
+  if (ans) {
+    rstudioapi::executeCommand("quitSession", quiet = T)
+  }
+  invisible(ans)
 }
--- R/remove.R
+++ R/remove.R
@@ -1,3 +1,7 @@
 remove_corretto <- function(os, ...) {
   UseMethod("remove_corretto")
 }
+
+remove_corretto.default <- function(os){
+  stop("no method for ", class(os)[1L])
+}
